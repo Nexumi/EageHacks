3ff0cfa63926d7c5ebbee98a4954c5340627f4e1
Nexumi <48866805+Nexumi@users.noreply.github.com>
2019-09-09 23:26:53 -0700

    Add files via upload

---
+++ assets/js/faq.js
@@ -0,0 +1,36 @@
+function dropa(x) {
+  document.getElementById("targeta").classList.toggle("showa");
+  document.getElementById("targeta").classList.toggle("hidea");
+}
+function dropb(x) {
+  document.getElementById("targetb").classList.toggle("showb");
+  document.getElementById("targetb").classList.toggle("hideb");
+}
+function dropc(x) {
+  document.getElementById("targetc").classList.toggle("showc");
+  document.getElementById("targetc").classList.toggle("hidec");
+}
+function dropd(x) {
+  document.getElementById("targetd").classList.toggle("showd");
+  document.getElementById("targetd").classList.toggle("hided");
+}
+function drope(x) {
+  document.getElementById("targete").classList.toggle("showe");
+  document.getElementById("targete").classList.toggle("hidee");
+}
+function dropf(x) {
+  document.getElementById("targetf").classList.toggle("showf");
+  document.getElementById("targetf").classList.toggle("hidef");
+}
+function dropg(x) {
+  document.getElementById("targetg").classList.toggle("showg");
+  document.getElementById("targetg").classList.toggle("hideg");
+}
+function droph(x) {
+  document.getElementById("targeth").classList.toggle("showh");
+  document.getElementById("targeth").classList.toggle("hideh");
+}
+function onLinkClick() {
+  document.getElementsById("works").scrollIntoView();
+  // will scroll to 4th h3 element
+}
